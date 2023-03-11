ae9a06b4fdd2c8353c0c2221643277182981905f
Reza Arif <90893301+Rezrfm@users.noreply.github.com>
2023-03-11 17:49:18 +0700

    Add files via upload

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$n1 = 10;
+$n2 = 7;
+$bn1 = 2;
+$bn2 = 8;
+$nama = "Reza Arif Maulana";
+$nim = 121103012;
+$hasil = ($n1 * $bn1) + ($n2 * $bn2);
+$tn = $n1 + $n2;
+
+echo "Total Nilai = {$hasil} <br>";
+echo "Hasil dari " . $n1 . " + " . $n2 . " adalah = {$tn} <br>";
+echo "Nama = {$nama} <br>";
+echo "NIM = {$nim}";
+
+?>
