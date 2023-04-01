a42305dd975512af16e584320222cb6b31c18a1b
Reza Arif <90893301+Rezrfm@users.noreply.github.com>
2023-04-01 14:20:30 +0700

    Add files via upload

---
+++ pertemuan6/input04.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengolahan Form</title>
+</head>
+<body>
+    <form action="proses04.php" method="POST" name="input">
+        Sahaba-sahabat Dekatku<br>
+        <input type="text" name="nama1"><br>
+        <input type="text" name="nama2"><br>
+        <input type="text" name="nama3"><br>
+        <input type="text" name="nama4"><br>
+        <input type="submit" name="Input" value="Input">
+    </form>
+</body>
+</html>
