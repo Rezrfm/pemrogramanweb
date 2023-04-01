4d8879bd25b5e0f25c8ea3050e61c58b3271ccee
Reza Arif <90893301+Rezrfm@users.noreply.github.com>
2023-04-01 13:50:20 +0700

    Add files via upload

---
+++ pertemuan5/if_else2.php
@@ -0,0 +1,12 @@
+<?php
+$user = "achmatin";
+$pass = "123";
+if ($user == "achmatin" && $pass == "123")
+{
+    echo "Login Berhasil";
+}
+else
+{
+    echo "Login Gagal";
+}
+?>
---
+++ pertemuan5/if_var.php
@@ -0,0 +1,11 @@
+<?php
+$user = "";
+if (!isset($user))
+{
+    echo "Variabel tidak /belum terbentuk";
+}
+else
+{
+    echo "Variabel ada";
+}
+?>
